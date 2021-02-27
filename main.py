5598552556a8d9522af97e7613f64e8a72920727
pam ram <pamramfam@gmail.com>
2021-02-27 03:19:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# FizzBuzz exercise implemented in Python 
+
+for num in range(1, 101):
+  if num % 3 == 0 and num % 5 == 0: 
+    print("FizzBuzz") 
+  elif num % 3 == 0: 
+    print("Fizz")
+  elif num % 5 == 0: 
+    print("Buzz")
+  else: 
+    print(num) 
+
+  
+
+
+
